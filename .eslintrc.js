353d1edb2e1c70d0d500e86c95949ad9b21d079d
Sang Yum <syum@amplifyfederal.com>
2025-07-21 07:04:05 -0700

    added .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/dist/**/*",
+    "/node_modules/**/*",
+  ],
+  plugins: [
+    "@typescript-eslint",
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+    "import/no-unresolved": 0,
+    "indent": ["error", 2],
+  },
+};
